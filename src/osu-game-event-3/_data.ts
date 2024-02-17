589089967fd43744934d5108a115c356f59a2ade
Ernestas Kluonis <ekgame1@gmail.com>
2024-02-17 23:56:49 +0200

    Order maps in the order they were played

--- src/osu-game-event-3/_data.ts
+++ src/osu-game-event-3/_data.ts
@@ -3,7 +3,7 @@ import scores from "./score_data.json" assert { type: "json" };
 export default {
   totalScores: scores.totalScores.sort((a,b) => b.score - a.score),
   beatmapScores: scores.beatmapScores
-    .sort((a,b) => b.scores.length - a.scores.length)
+    // .sort((a,b) => b.scores.length - a.scores.length)
     .map(game => ({
       ...game,
       scores: game.scores.sort((a,b) => b.score - a.score),
